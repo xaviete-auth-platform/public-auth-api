33aa053b2e06e111dfe4fa198fac410d570934d4
xavigrausebastian <xavier.grau@kreatings.com>
2022-08-18 20:32:21 +0200

    IMPLEMENTS requestLog to login controller

--- src/v1/controllers/login.ts
+++ src/v1/controllers/login.ts
@@ -2,44 +2,81 @@ import { userExist, validateUser } from "../services/user";
 import { Request, Response } from "express";
 import { HttpStatusCode as status } from "../config/status";
 import {validatePassword} from "../services/auth";
+import {requestLog} from "../services/request";
 
 export default async function (req: Request, res: Response) {
 
-    // Validate user data
-    const validate = await validateUser(req.body ? req.body : {});
+    try {
 
-    if (validate.status !== status.OK) {
-        return res.status(validate.status).json(validate);
+        // Validate user data
+        const validate = await validateUser(req.body ? req.body : {});
+
+        if (validate.status !== status.OK) {
+
+            // Register the request
+            await requestLog(validate);
+
+            return res.status(validate.status).json(validate);
+        }
+
+        const {email, password} = req.body;
+
+        // Get project id form token
+        const project_id = res.locals.token.id;
+
+        // Validate if user exist with the given email and project_id
+        const user = await userExist(project_id, email);
+
+        if (!user) {
+
+            let response = {
+                status: status.NOT_ACCEPTABLE,
+                message: 'Invalid credentials. User no exist'
+            }
+
+            // Register the request
+            await requestLog(response);
+
+            return res.status(status.NOT_ACCEPTABLE).send(response);
+        }
+
+        // Validate if password is correct
+        if (!validatePassword(password, user.password.toString())) {
+
+            let response = {
+                status: status.NOT_ACCEPTABLE,
+                message: 'Invalid credentials'
+            }
+
+            // Register the request
+            await requestLog(response);
+
+            return res.status(status.NOT_ACCEPTABLE).send(response);
+        }
+
+        let response = {
+            status: status.OK,
+            message: 'Login successfully',
+            user: user
+        }
+
+        // Register the request
+        await requestLog(response);
+
+        res.status(status.OK).send(response);
+
+    } catch (e) {
+
+        let response = {
+            status: status.INTERNAL_SERVER_ERROR,
+            message: e.message,
+            error: e
+        }
+
+        // Register the request
+        await requestLog(response);
+
+        res.status(status.INTERNAL_SERVER_ERROR).send(response);
+
     }
-
-    const {email, password} = req.body;
-
-    // Get project id form token
-    const project_id = res.locals.token.id;
-
-    // Validate if user exist with the given email and project_id
-    const user = await userExist(project_id, email);
-
-    if (!user) {
-
-        return res.status(status.NOT_ACCEPTABLE).send({
-            status: status.NOT_ACCEPTABLE,
-            message: 'Invalid credentials. User no exist'
-        });
-    }
-
-    // Validate if password is correct
-    if (!validatePassword(password, user.password.toString())) {
-
-        return res.status(status.NOT_ACCEPTABLE).send({
-            status: status.NOT_ACCEPTABLE,
-            message: 'Invalid credentials'
-        });
-    }
-
-    res.status(status.OK).send({
-        status: status.OK,
-        message: 'Login successfully',
-        user: user
-    });
 }
